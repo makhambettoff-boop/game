277983902b2af336777f7b2bc5a7fcde017be14f
Rasul <makhambettoff@gmail.com>
2026-01-01 23:57:46 +0600

    jana bagdarlama

--- inndex.js
+++ inndex.js
@@ -1,14 +1,20 @@
-function Calculator (a, b, c){
-    if (Number.isNaN(a)|| Number.isNaN(c)) return "ошибка"
-if (b=== "+"){
-    return a+c}
-if (b=== "-"){
-    return a-c}
-if (b=== "*"){
-    return a*C}
-    if (b=== "/") {
-        if (c=== 0) return "ошибка"
-        return a/c
-    }
+// function Calculator (a, b, c){
+//     if (Number.isNaN(a)|| Number.isNaN(c)) return "ошибка"
+// if (b=== "+"){
+//     return a+c}
+// if (b=== "-"){
+//     return a-c}
+// if (b=== "*"){
+//     return a*C}
+//     if (b=== "/") {
+//         if (c=== 0) return "ошибка"
+//         return a/c
+//     }
         
+// };
+function (email, password,){
+    if (email === "") return "поле не должно быть пустым"
+    if (!email.includes ("@")) return "@ обязательный символ"
+if (password === "") return "пароль не должен быть пустым"
+
 };
