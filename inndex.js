b46b5fe7d4bf84c121f3044ff18382fd88e26ed1
Rasul <makhambettoff@gmail.com>
2026-01-02 23:49:34 +0600

    пицерия новый проект

--- inndex.js
+++ inndex.js
@@ -1,20 +1,26 @@
-// function Calculator (a, b, c){
-//     if (Number.isNaN(a)|| Number.isNaN(c)) return "ошибка"
-// if (b=== "+"){
-//     return a+c}
-// if (b=== "-"){
-//     return a-c}
-// if (b=== "*"){
-//     return a*C}
-//     if (b=== "/") {
-//         if (c=== 0) return "ошибка"
-//         return a/c
-//     }
+    // async function OrderDoner(){
+    //     try { 
+    //         await new Promise((resolve) =>setTimeout(resolve, 3000))
+    //  console.log("заказ онделуде");
+    //   throw new Error("произошла ошибка")
+    //    await new Promise((resolve)=>setTimeout(resolve, 2000))
+    //    console.log("ваш заказ успешно принят")
         
-// };
-function (email, password,){
-    if (email === "") return "поле не должно быть пустым"
-    if (!email.includes ("@")) return "@ обязательный символ"
-if (password === "") return "пароль не должен быть пустым"
+    
+    //     }catch(error){
 
-};
+    //         console.error("произошла ошибка")
+    //     }
+        
+    // };
+    // OrderDoner();
+function wait(ms){
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+    async function pizza(){
+await wait(2000)
+    const ok=Math.random()>0.5
+    if(!ok) return "интернет не работает"
+    return "пицерия жумыс истеп тур"
+    
+    };
